48ac9faba9efb49a70e4d85d943065ab4c0f9ac4
sa-GT <habeeb.alafeef6@gmail.com>
2025-06-15 16:56:51 +0300

    cmd

---
+++ TimeManagement/Controllers/Habib/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TimeManagement.Controllers.Habib
+{
+	public class UserController : Controller
+	{
+		public IActionResult LandingPage()
+		{
+			return View();
+		}
+	}
+}
